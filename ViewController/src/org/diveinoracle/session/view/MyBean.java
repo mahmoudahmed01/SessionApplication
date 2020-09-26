f9da8a4826561b092521aaa1d1a46f884219d25e
Mahmoud Elsayed <mahmoud_ahmed01@yahoo.com>
2020-09-26 06:23:02 +0400

    Part 2

--- ViewController/src/org/diveinoracle/session/view/MyBean.java
+++ ViewController/src/org/diveinoracle/session/view/MyBean.java
@@ -1,5 +1,9 @@
 package org.diveinoracle.session.view;
 
+import java.util.HashMap;
+
+import java.util.Map;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
@@ -23,14 +27,35 @@ public class MyBean {
         // Add event code here...
         SessionUtil.storeOnSession("studentName", studentName);
         
+        //Map 
+        Map studentAsMap = new HashMap<String,String>();
+        studentAsMap.put("Name", studentName);
+        SessionUtil.storeOnSession("studentAsMap", studentAsMap);
+        
+        //Array of String
+        String[] students = { "0)" + studentName ,
+                              "1)" + studentName};
+        SessionUtil.storeOnSession("studentAsArray", students);
+        
+                        
+        //Class Object
+        Student student = new Student(studentName);
+        SessionUtil.storeOnSession("studentAsObj", student);
+        
+        
     }
 
     public void onClickReadDataBtn(ActionEvent actionEvent) {
         // Add event code here...
         String studentName = (String)SessionUtil.getFromSession("studentName");
+        Map studentAsMap = (Map) SessionUtil.getFromSession("studentAsMap");
+        String[] studentAsArray = (String [])SessionUtil.getFromSession("studentAsArray");
+        Student studentAsObj = (Student) SessionUtil.getFromSession("studentAsObj");
         
-        String msgText  =  "Student Name in Session is " + studentName ;
-
+        String msgText = " Map student name is " + studentAsMap.get("Name") +
+                         " , Student in Array [0] " + studentAsArray[0] +
+                         " , Student in Class Object " + studentAsObj.getStudentName();
+        
         FacesMessage facesMsg = new FacesMessage(msgText);
         FacesContext.getCurrentInstance().addMessage(null, facesMsg);
     }
---
+++ ViewController/src/org/diveinoracle/session/view/Student.java
@@ -0,0 +1,24 @@
+package org.diveinoracle.session.view;
+
+public class Student {
+    String studentName;
+    
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+    
+    public Student() {
+        super();
+    }
+    
+    public Student(String studentName) {
+        this.studentName = studentName;
+    }
+    
+
+    
+}
